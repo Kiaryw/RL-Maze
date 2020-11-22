a1fd655a9cb413310f82983b2c776c4ab15b9397
Kiaryw <kiarywong@gmail.com>
2020-11-22 21:19:00 +0800

    add utils

---
+++ MonteCarlo/utils.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+from mcts_utils import StateNode, StateActionNode
+
+
+class ModelWrapper(object):
+    # wrap env model to fit mcts
+    def __init__(self, env):
+        self.env = env
+
+    def step(self, state, action):
+        self.env.load_states(state)
+        _, reward, done, status = self.env.step(action)
+        state_nxt = self.env.save_states()
+
+        return state_nxt, reward, done
